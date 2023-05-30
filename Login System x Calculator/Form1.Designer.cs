79be74983607c295e0387f4066f67959e388fe22
Rustam Hasanli <rustamh2006@gmail.com>
2023-05-30 21:37:55 +0400

    caluclator only

---
+++ Login System x Calculator/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace Login_System_x_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Numbers = new System.Windows.Forms.GroupBox();
+            this.number6 = new System.Windows.Forms.Button();
+            this.number5 = new System.Windows.Forms.Button();
+            this.del = new System.Windows.Forms.Button();
+            this.number9 = new System.Windows.Forms.Button();
+            this.number8 = new System.Windows.Forms.Button();
+            this.number7 = new System.Windows.Forms.Button();
+            this.number4 = new System.Windows.Forms.Button();
+            this.equals = new System.Windows.Forms.Button();
+            this.reset = new System.Windows.Forms.Button();
+            this.point = new System.Windows.Forms.Button();
+            this.number1 = new System.Windows.Forms.Button();
+            this.number0 = new System.Windows.Forms.Button();
+            this.divide = new System.Windows.Forms.Button();
+            this.number2 = new System.Windows.Forms.Button();
+            this.multiply = new System.Windows.Forms.Button();
+            this.number3 = new System.Windows.Forms.Button();
+            this.subtract = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.ResultMenu = new System.Windows.Forms.RichTextBox();
+            this.Numbers.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Numbers
+            // 
+            this.Numbers.Controls.Add(this.number6);
+            this.Numbers.Controls.Add(this.number5);
+            this.Numbers.Controls.Add(this.del);
+            this.Numbers.Controls.Add(this.number9);
+            this.Numbers.Controls.Add(this.number8);
+            this.Numbers.Controls.Add(this.number7);
+            this.Numbers.Controls.Add(this.number4);
+            this.Numbers.Controls.Add(this.equals);
+            this.Numbers.Controls.Add(this.reset);
+            this.Numbers.Controls.Add(this.point);
+            this.Numbers.Controls.Add(this.number1);
+            this.Numbers.Controls.Add(this.number0);
+            this.Numbers.Controls.Add(this.divide);
+            this.Numbers.Controls.Add(this.number2);
+            this.Numbers.Controls.Add(this.multiply);
+            this.Numbers.Controls.Add(this.number3);
+            this.Numbers.Controls.Add(this.subtract);
+            this.Numbers.Controls.Add(this.add);
+            this.Numbers.Location = new System.Drawing.Point(212, 80);
+            this.Numbers.Name = "Numbers";
+            this.Numbers.Size = new System.Drawing.Size(391, 306);
+            this.Numbers.TabIndex = 1;
+            this.Numbers.TabStop = false;
+            this.Numbers.Text = "Numbers";
+            // 
+            // number6
+            // 
+            this.number6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.number6.Location = new System.Drawing.Point(209, 91);
+            this.number6.Name = "number6";
+            this.number6.Size = new System.Drawing.Size(66, 37);
+            this.number6.TabIndex = 0;
+            this.number6.Text = "6";
+            this.number6.UseVisualStyleBackColor = false;
+            this.number6.Click += new System.EventHandler(this.number6_Click);
+            // 
+            // number5
+            // 
+            this.number5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.number5.Location = new System.Drawing.Point(127, 91);
+            this.number5.Name = "number5";
+            this.number5.Size = new System.Drawing.Size(66, 37);
+            this.number5.TabIndex = 0;
+            this.number5.Text = "5";
+            this.number5.UseVisualStyleBackColor = false;
+            this.number5.Click += new System.EventHandler(this.number5_Click);
+            // 
+            // del
+            // 
+            this.del.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.del.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.del.Location = new System.Drawing.Point(291, 29);
+            this.del.Name = "del";
+            this.del.Size = new System.Drawing.Size(66, 37);
+            this.del.TabIndex = 0;
+            this.del.Text = "Del";
+            this.del.UseVisualStyleBackColor = false;
+            this.del.Click += new System.EventHandler(this.del_Click);
+            // 
+            // number9
+            // 
+            this.number9.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.number9.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.number9.Location = new System.Drawing.Point(209, 29);
+            this.number9.Name = "number9";
+            this.number9.Size = new System.Drawing.Size(66, 37);
+            this.number9.TabIndex = 0;
+            this.number9.Text = "9";
+            this.number9.UseVisualStyleBackColor = false;
+            this.number9.Click += new System.EventHandler(this.number9_Click);
+            // 
+            // number8
+            // 
+            this.number8.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.number8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.number8.Location = new System.Drawing.Point(127, 29);
+            this.number8.Name = "number8";
+            this.number8.Size = new System.Drawing.Size(66, 37);
+            this.number8.TabIndex = 0;
+            this.number8.Text = "8";
+            this.number8.UseVisualStyleBackColor = false;
+            this.number8.Click += new System.EventHandler(this.number8_Click);
+            // 
+            // number7
+            // 
+            this.number7.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.number7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.number7.Location = new System.Drawing.Point(45, 29);
+            this.number7.Name = "number7";
+            this.number7.Size = new System.Drawing.Size(66, 37);
+            this.number7.TabIndex = 0;
+            this.number7.Text = "7";
+            this.number7.UseVisualStyleBackColor = false;
+            this.number7.Click += new System.EventHandler(this.number7_Click);
+            // 
+            // number4
+            // 
+            this.number4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.number4.Location = new System.Drawing.Point(45, 91);
+            this.number4.Name = "number4";
+            this.number4.Size = new System.Drawing.Size(66, 37);
+            this.number4.TabIndex = 0;
+            this.number4.Text = "4";
+            this.number4.UseVisualStyleBackColor = false;
+            this.number4.Click += new System.EventHandler(this.number4_Click);
+            // 
+            // equals
+            // 
+            this.equals.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.equals.Location = new System.Drawing.Point(209, 253);
+            this.equals.Name = "equals";
+            this.equals.Size = new System.Drawing.Size(148, 33);
+            this.equals.TabIndex = 0;
+            this.equals.Text = "=";
+            this.equals.UseVisualStyleBackColor = false;
+            this.equals.Click += new System.EventHandler(this.equals_Click);
+            // 
+            // reset
+            // 
+            this.reset.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.reset.Location = new System.Drawing.Point(45, 253);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(148, 33);
+            this.reset.TabIndex = 0;
+            this.reset.Text = "Reset";
+            this.reset.UseVisualStyleBackColor = false;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // point
+            // 
+            this.point.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.point.Location = new System.Drawing.Point(45, 205);
+            this.point.Name = "point";
+            this.point.Size = new System.Drawing.Size(66, 33);
+            this.point.TabIndex = 0;
+            this.point.Text = ".";
+            this.point.UseVisualStyleBackColor = false;
+            this.point.Click += new System.EventHandler(this.point_Click);
+            // 
+            // number1
+            // 
+            this.number1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.number1.Location = new System.Drawing.Point(45, 151);
+            this.number1.Name = "number1";
+            this.number1.Size = new System.Drawing.Size(66, 33);
+            this.number1.TabIndex = 0;
+            this.number1.Text = "1";
+            this.number1.UseVisualStyleBackColor = false;
+            this.number1.Click += new System.EventHandler(this.number1_Click);
+            // 
+            // number0
+            // 
+            this.number0.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.number0.Location = new System.Drawing.Point(127, 205);
+            this.number0.Name = "number0";
+            this.number0.Size = new System.Drawing.Size(66, 33);
+            this.number0.TabIndex = 0;
+            this.number0.Text = "0";
+            this.number0.UseVisualStyleBackColor = false;
+            this.number0.Click += new System.EventHandler(this.number0_Click);
+            // 
+            // divide
+            // 
+            this.divide.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.divide.Location = new System.Drawing.Point(209, 205);
+            this.divide.Name = "divide";
+            this.divide.Size = new System.Drawing.Size(66, 33);
+            this.divide.TabIndex = 0;
+            this.divide.Text = "/";
+            this.divide.UseVisualStyleBackColor = false;
+            this.divide.Click += new System.EventHandler(this.divide_Click);
+            // 
+            // number2
+            // 
+            this.number2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.number2.Location = new System.Drawing.Point(127, 151);
+            this.number2.Name = "number2";
+            this.number2.Size = new System.Drawing.Size(66, 33);
+            this.number2.TabIndex = 0;
+            this.number2.Text = "2";
+            this.number2.UseVisualStyleBackColor = false;
+            this.number2.Click += new System.EventHandler(this.number2_Click);
+            // 
+            // multiply
+            // 
+            this.multiply.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.multiply.Location = new System.Drawing.Point(291, 205);
+            this.multiply.Name = "multiply";
+            this.multiply.Size = new System.Drawing.Size(66, 33);
+            this.multiply.TabIndex = 0;
+            this.multiply.Text = "*";
+            this.multiply.UseVisualStyleBackColor = false;
+            this.multiply.Click += new System.EventHandler(this.multiply_Click);
+            // 
+            // number3
+            // 
+            this.number3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.number3.Location = new System.Drawing.Point(209, 151);
+            this.number3.Name = "number3";
+            this.number3.Size = new System.Drawing.Size(66, 33);
+            this.number3.TabIndex = 0;
+            this.number3.Text = "3";
+            this.number3.UseVisualStyleBackColor = false;
+            this.number3.Click += new System.EventHandler(this.number3_Click);
+            // 
+            // subtract
+            // 
+            this.subtract.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.subtract.Location = new System.Drawing.Point(291, 151);
+            this.subtract.Name = "subtract";
+            this.subtract.Size = new System.Drawing.Size(66, 33);
+            this.subtract.TabIndex = 0;
+            this.subtract.Text = "-";
+            this.subtract.UseVisualStyleBackColor = false;
+            this.subtract.Click += new System.EventHandler(this.subtract_Click);
+            // 
+            // add
+            // 
+            this.add.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.add.Location = new System.Drawing.Point(291, 91);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(66, 37);
+            this.add.TabIndex = 0;
+            this.add.Text = "+";
+            this.add.UseVisualStyleBackColor = false;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // ResultMenu
+            // 
+            this.ResultMenu.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ResultMenu.Font = new System.Drawing.Font("Yu Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ResultMenu.Location = new System.Drawing.Point(212, 21);
+            this.ResultMenu.Name = "ResultMenu";
+            this.ResultMenu.Size = new System.Drawing.Size(391, 53);
+            this.ResultMenu.TabIndex = 2;
+            this.ResultMenu.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(803, 450);
+            this.Controls.Add(this.ResultMenu);
+            this.Controls.Add(this.Numbers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Numbers.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private GroupBox Numbers;
+        private Button number6;
+        private Button number5;
+        private Button del;
+        private Button number9;
+        private Button number8;
+        private Button number7;
+        private Button number4;
+        private Button equals;
+        private Button reset;
+        private Button point;
+        private Button number1;
+        private Button number0;
+        private Button divide;
+        private Button number2;
+        private Button multiply;
+        private Button number3;
+        private Button subtract;
+        private Button add;
+        private RichTextBox ResultMenu;
+    }
+}
---
+++ Login System x Calculator/Form1.cs
@@ -0,0 +1,217 @@
+using System.Reflection.Emit;
+
+namespace Login_System_x_Calculator
+{
+    public partial class Form1 : Form
+    {
+        string firstNumber = "";
+        string secondNumber = "";
+        string operation = "";
+        double result = 0;
+        string currentNumber = "0";
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void del_Click(object sender, EventArgs e)
+        {
+            currentNumber = currentNumber.Substring(0, currentNumber.Length - 1);
+            ResultMenu.Text = currentNumber;
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+            firstNumber = "";
+            secondNumber = "";
+            operation = "";
+            ResultMenu.Text = "";
+            currentNumber = "";
+        }
+
+        private void equals_Click(object sender, EventArgs e)
+        {
+            
+            if (operation != "")
+            {
+                firstNumber = currentNumber.Substring(0, currentNumber.IndexOf(operation));
+                secondNumber = currentNumber.Substring(currentNumber.IndexOf(operation) + 1);
+            }
+            switch (operation)
+            {
+                case "+":
+                    result = Convert.ToDouble(firstNumber) + Convert.ToDouble(secondNumber);
+                    break;
+                case "-":
+                    result = Convert.ToDouble(firstNumber) - Convert.ToDouble(secondNumber);
+                    break;
+                case "*":
+                    result = Convert.ToDouble(firstNumber) * Convert.ToDouble(secondNumber);
+                    break ;
+                case "/":
+                    if (secondNumber == "0") MessageBox.Show("No Zero Division");
+                    else result = Convert.ToDouble(firstNumber) / Convert.ToDouble(secondNumber);
+                    break;
+                default:
+                    break;
+            }
+            currentNumber = result.ToString();
+            ResultMenu.Text = currentNumber;
+            firstNumber = "";
+            secondNumber = "";
+            operation = "";
+            
+
+        }
+
+        private void number1_Click(object sender, EventArgs e)
+        {
+            currentNumber += "1";
+            if (currentNumber[0] == '0' && currentNumber[1] != ',' && currentNumber.Length > 1) currentNumber = currentNumber.Substring(1);
+            ResultMenu.Text = currentNumber;
+        }
+
+        private void number2_Click(object sender, EventArgs e)
+        {
+            currentNumber += "2";
+            if (currentNumber[0] == '0' && currentNumber[1] != ',' && currentNumber.Length > 1) currentNumber = currentNumber.Substring(1);
+            ResultMenu.Text = currentNumber;
+        }
+
+        private void number3_Click(object sender, EventArgs e)
+        {
+            currentNumber += "3";
+            if (currentNumber[0] == '0' && currentNumber[1] != ',' && currentNumber.Length > 1) currentNumber = currentNumber.Substring(1);
+            ResultMenu.Text = currentNumber;
+
+        }
+
+        private void number4_Click(object sender, EventArgs e)
+        {
+            currentNumber += "4";
+            if (currentNumber[0] == '0' && currentNumber[1] != ',' && currentNumber.Length > 1) currentNumber = currentNumber.Substring(1);
+            ResultMenu.Text = currentNumber;
+
+        }
+
+        private void number5_Click(object sender, EventArgs e)
+        {
+            currentNumber += "5";
+            if (currentNumber[0] == '0' && currentNumber[1] != ',' && currentNumber.Length > 1) currentNumber = currentNumber.Substring(1);
+            ResultMenu.Text = currentNumber;
+
+        }
+
+        private void number6_Click(object sender, EventArgs e)
+        {
+            currentNumber += "6";
+            if (currentNumber[0] == '0' && currentNumber[1] != ',' && currentNumber.Length > 1) currentNumber = currentNumber.Substring(1);
+            ResultMenu.Text = currentNumber;
+
+        }
+
+        private void number7_Click(object sender, EventArgs e)
+        {
+            currentNumber += "7";
+            if (currentNumber[0] == '0' && currentNumber[1] != ',' && currentNumber.Length > 1) currentNumber = currentNumber.Substring(1);
+            ResultMenu.Text = currentNumber;
+
+        }
+
+        private void number8_Click(object sender, EventArgs e)
+        {
+            currentNumber += "8";
+            if (currentNumber[0] == '0' && currentNumber[1]!=',' && currentNumber.Length > 1) currentNumber=currentNumber.Substring(1);
+            ResultMenu.Text = currentNumber;
+
+        }
+
+        private void number9_Click(object sender, EventArgs e)
+        {
+            currentNumber += "9";
+            if (currentNumber[0] == '0' && currentNumber[1] != ',' && currentNumber.Length > 1) currentNumber = currentNumber.Substring(1);
+            ResultMenu.Text = currentNumber;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ResultMenu.Text = currentNumber;
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            if(operation!="") {
+                equals_Click(sender, e); 
+            }
+            operation = "+";
+            if (currentNumber.EndsWith("-") || currentNumber.EndsWith("+") || currentNumber.EndsWith("*") || currentNumber.EndsWith("/"))
+            {
+                currentNumber = currentNumber.Replace(currentNumber[currentNumber.Length - 1], '+');
+            }
+            else currentNumber += operation;
+            ResultMenu.Text = currentNumber;
+        }
+
+        private void subtract_Click(object sender, EventArgs e)
+        {
+            if (operation != "")
+            {
+                equals_Click(sender, e);
+            }
+            operation = "-";
+            if (currentNumber.EndsWith("-") || currentNumber.EndsWith("+") || currentNumber.EndsWith("*") || currentNumber.EndsWith("/"))
+            {
+                currentNumber = currentNumber.Replace(currentNumber[currentNumber.Length - 1], '-');
+            }
+            else currentNumber += operation;
+            ResultMenu.Text = currentNumber;
+        }
+
+        private void multiply_Click(object sender, EventArgs e)
+        {
+            if (operation != "")
+            {
+                equals_Click(sender, e);
+            }
+            operation = "*";
+            if (currentNumber.EndsWith("-") || currentNumber.EndsWith("+") || currentNumber.EndsWith("*") || currentNumber.EndsWith("/"))
+            {
+                currentNumber = currentNumber.Replace(currentNumber[currentNumber.Length - 1], '*');
+            }
+            else
+                currentNumber += operation;
+            ResultMenu.Text = currentNumber;
+        }
+
+        private void divide_Click(object sender, EventArgs e)
+        {
+            if (operation != "")
+            {
+                equals_Click(sender, e);
+            }
+            operation ="/";
+            if (currentNumber.EndsWith("-") || currentNumber.EndsWith("+") || currentNumber.EndsWith("*") || currentNumber.EndsWith("/"))
+            {
+                currentNumber = currentNumber.Replace(currentNumber[currentNumber.Length - 1], '/');
+            }
+            else
+                currentNumber += operation;
+            ResultMenu.Text = currentNumber;
+        }
+
+        private void point_Click(object sender, EventArgs e)
+        {
+            currentNumber += ",";
+            ResultMenu.Text = currentNumber;
+        }
+
+        private void number0_Click(object sender, EventArgs e)
+        {
+            
+            if(currentNumber!="0")currentNumber += "0";
+            ResultMenu.Text = currentNumber;
+        }
+    }
+}
